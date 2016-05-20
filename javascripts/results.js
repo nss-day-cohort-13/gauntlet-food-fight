7965f2dbc226494d7e14e4491c6794d0f19e66a6
Hannah Hall <hannahkhall13@gmail.com>
2016-05-20 13:16:08 -0500

    fixed both dead

--- javascripts/results.js
+++ javascripts/results.js
@@ -10,18 +10,18 @@ var FoodFight = (function (results) {
 	results.checkForDeath = function(hero, enemy) {
 		if (hero <= 0 || enemy <= 0) {
 			modal.show();
-			if (hero <= 0) {
+			if(hero <= 0 && enemy <= 0) {
+				resultsDiv.append(`<p class="resultP">You have both been defeated. Would you like to play again?`);
+			}
+			else if (hero <= 0) {
 				resultsDiv.append(`<p class="resultP">Sorry you've been defeated. Would you like to play again?</p>`);
 			}
 			else if(enemy <= 0) {
 				resultsDiv.append(`<p class="resultP">You are the victor! Would you like to play again?</p>`);
-			} else {
-				resultsDiv.append(`<p class="resultP">You have both been defeated. Would you like to play again?`);
 			}
 		}
 		resetBtn.click(playAgain);
 	}
-
 
 	return results;
 
