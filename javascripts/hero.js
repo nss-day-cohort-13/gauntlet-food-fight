ac79816cc953a63cae024e0b12a811167e7a98a6
Hannah Hall <hannahkhall13@gmail.com>
2016-05-20 10:09:37 -0500

    made player1 function public

--- javascripts/hero.js
+++ javascripts/hero.js
@@ -1,13 +1,13 @@
 "use strict";
 var FoodFight = (function(hero){
 
-	function Player (name) {
+	hero.Player1 = function (name) {
 		this.health = 50;
 		this.attack = 10;
 		this.name = name;
 	}
 
-	hero.player1 = new Player("Larry");
+	hero.player1 = new hero.Player1("Larry");
 
 	return hero
 
