1dd0f008276750e3229a2df3eecb31d7acbae987
Keith <keithbradley1@msn.com>
2016-05-20 10:15:49 -0500

    syntax error changes

--- javascripts/enemy.js
+++ javascripts/enemy.js
@@ -1,12 +1,12 @@
 var FoodFight = (function(enemy) {
 
   enemy.Player2 = function () {
-    this.health = 50
-    this.attack = 10
+    this.health = 50;
+    this.attack = 10;
   }
 
   enemy.player2 = new enemy.Player2();
-  return enemy
+  return enemy;
 
 })(FoodFight ||{})
 
