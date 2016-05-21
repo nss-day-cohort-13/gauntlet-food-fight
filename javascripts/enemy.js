2118e4dacfbfdd3aafbf5d6010572cc281047149
Keith <keithbradley1@msn.com>
2016-05-21 12:29:57 -0500

    create enemy objects to randomize

--- javascripts/enemy.js
+++ javascripts/enemy.js
@@ -1,24 +1,40 @@
+"use strict";
+
 var FoodFight = (function(enemy) {
 
-  enemy.Player2 = function () {
+  enemy.Player2 = function (name) {
+    this.name = name;
     this.health = 50;
     this.attack = Math.floor(Math.random()*20 + 1) + 1;
-    this.name = ["Paula Dean":"Heart Attack! She throws a stick of butter", "Gordon Ramsey": "Verbal Assault! He tells you you should kill yourself.", "Mrs. Lovett": "Meat Pie Attack! You ate some bad pie.", "Guy Fiere": "Offensive Appearance! You are sick from watching him eat."];
-    this.generateEnemy = function() {}
+    this.allowedEnemies = [enemy.Paula, enemy.Gordon, enemy.Lovett, enemy.Guy];
+    this.generateEnemy = function() {
     // get a random index from the name array
-    var random = Math.round(Math.random() * (this.name.length -1));
+    var random = Math.round(Math.random() * (this.allowedEnemies.length -1));
 
     //get the string at the index
-    var randomEnemy = this.name[random];
+    var randomEnemy = this.allowedEnemies[random];
 
     //compose the corresponding enemy into the player object
-    this.name = new FoodFight.enemy[randomEnemy]();
-    return this.
-  }
+
+    this.randomEnemy = randomEnemy;
+    };
+  };
 
   enemy.player2 = new enemy.Player2();
+
+  enemy.Paula = new enemy.Player2("Paula Dean");
+  enemy.Paula.attackSpecialty = "Heart Attack! She throws a stick of butter";
+
+  enemy.Gordon = new enemy.Player2("Gordon Ramsey");
+  enemy.Gordon.attackSpecialty = "Verbal Assault! He tells you to kill yourself.";
+
+  enemy.Lovett = new enemy.Player2("Mrs. Lovett");
+  enemy.Lovett.attackSpecialty = "Meat Pie Attack! You ate some bad pie.";
+
+  enemy.Guy = new enemy.Player2("Guy Fiere");
+  enemy.Guy.attackSpecialty = "Offensive Appearance! You are sick from watching Guy Fiere eat.";
+
   return enemy;
+})(FoodFight ||{});
 
-})(FoodFight ||{})
-
-console.log(FoodFight.player2);
+console.log("FoodFight", FoodFight);
