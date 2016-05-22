c2546319f6c1ff2482ac048aea536a3197dde68a
Keith <keithbradley1@msn.com>
2016-05-22 13:33:19 -0500

    added return, and some special attacks

--- javascripts/enemy.js
+++ javascripts/enemy.js
@@ -7,26 +7,32 @@ var FoodFight = (function(enemy) {
     this.health = 50;
     this.attack = Math.floor(Math.random()*20 + 1) + 1;
     this.allowedEnemies = [enemy.Paula, enemy.Gordon, enemy.Lovett, enemy.Guy];
-    this.generateEnemy = function() {
-    // get a random index from the name array
-    var random = Math.round(Math.random() * (this.allowedEnemies.length -1));
+  };
+  enemy.Player2.generateEnemy = function() {
+  // get a random index from the name array
+  var random = Math.round(Math.random() * (this.allowedEnemies.length -1));
 
-    //get the string at the index
-    var randomEnemy = this.allowedEnemies[random];
+  //get the string at the index
+  var randomEnemy = this.allowedEnemies[random];
 
-    //compose the corresponding enemy into the player object
+  //compose the corresponding enemy into the player object
 
-    this.name = randomEnemy.name;
-    };
+  // this.name = randomEnemy.name;
+  return randomEnemy;
   };
 
-  enemy.player2 = new FoodFight.Player2();
+  enemy.Player2 = new FoodFight.Player2();
 
   enemy.Paula = new enemy.Player2("Paula Dean");
   enemy.Paula.attackSpecialty = "Heart Attack! She throws a stick of butter";
+  enemy.Paula.attackSpecialty = "Bacon Attack! She wraps you in bacon";
+  enemy.Paula.attackSpecialty = "Insensitivity Attack! She makes racist jokes at you.";
 
   enemy.Gordon = new enemy.Player2("Gordon Ramsey");
-  enemy.Gordon.attackSpecialty = "Verbal Assault! He tells you to kill yourself.";
+  enemy.Gordon.attackSpecialty = "Verbal Assault! He tells you to jump in the oven.";
+  enemy.Gordon.attackSpecialty = "Verbal Assault! He puts bread on your head and calls you an idiot sandwich.";
+  enemy.Gordon.attackSpecialty = "Verbal Assault! He calls you a panini head.";
+  enemy.Gordon.attackSpecialty = "Verbal Assault! He calls you a useless sack of Yankee Doodle Dandy Shite.";
 
   enemy.Lovett = new enemy.Player2("Mrs. Lovett");
   enemy.Lovett.attackSpecialty = "Meat Pie Attack! You ate some bad pie.";
