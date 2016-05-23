5da260c65c098f2c0aa28bdcec1084d34ed26e74
Keith <keithbradley1@msn.com>
2016-05-23 09:48:42 -0500

    edit attack speialty bugs

--- javascripts/enemy.js
+++ javascripts/enemy.js
@@ -9,10 +9,10 @@ var FoodFight = (function(enemy) {
     this.allowedEnemies = [enemy.Paula, enemy.Gordon, enemy.Lovett, enemy.Guy];
     this.bonus = [];
 
-    this.paulaSpecialty = [enemy.Paula.attackSpecialty, enemy.Paula.attackSpecialty2, enemy.Paula.attackSpecialty3];
-    this.gordonSpecialty = [enemy.Gordon.attackSpecialty, enemy.Gordon.attackSpecialty2, enemy.Gordon.attackSpecialty3];
-    this.lovettSpecialty = [enemy.Lovett.attackSpecialty, enemy.Lovett.attackSpecialty2, enemy.Lovett.attackSpecialty3];
-    this.guySpecialty = [enemy.Guy.attackSpecialty, enemy.Guy.attackSpecialty2, enemy.Guy.attackSpecialty3];
+    this.paulaSpecialty = [];
+    this.gordonSpecialty = [];
+    this.lovettSpecialty = [];
+    this.guySpecialty = [];
     this.weapon = ['knife', 'Meat Tenderizer', 'Chinese Takeout', 'Frying Pan'];
 
     this.generateEnemy = function() {
@@ -31,21 +31,21 @@ var FoodFight = (function(enemy) {
 
 
   enemy.Paula = new enemy.Player2("Paula Dean");
-  enemy.Paula.attackSpecialty = "Heart Attack! She throws a stick of butter";
-  enemy.Paula.attackSpecialty2 = "Bacon Attack! She wraps you in bacon";
-  enemy.Paula.attackSpecialty3 = "Insensitivity Attack! She hurls racist jokes at you.";
+  enemy.Paula.paulaSpecialty.push("Heart Attack! She throws a stick of butter");
+  enemy.Paula.paulaSpecialty.push("Bacon Attack! She wraps you in bacon");
+  enemy.Paula.paulaSpecialty.push("Insensitivity Attack! She hurls racist jokes at you.");
 
   enemy.Gordon = new enemy.Player2("Gordon Ramsey");
-  enemy.Gordon.attackSpecialty = "Verbal Assault! He tells you to jump in the oven.";
-  enemy.Gordon.attackSpecialty2 = "Verbal Assault! He puts bread on your head and calls you an idiot sandwich.";
-  enemy.Gordon.attackSpecialty3 = "Verbal Assault! He calls you a panini head.";
-  enemy.Gordon.attackSpecialty4 = "Verbal Assault! He calls you a useless sack of Yankee Doodle Dandy Shite.";
+  enemy.Gordon.gordonSpecialty.push("Verbal Assault! He tells you to jump in the oven.");
+  enemy.Gordon.gordonSpecialty.push("Verbal Assault! He puts bread on your head and calls you an idiot sandwich.");
+  enemy.Gordon.gordonSpecialty.push("Verbal Assault! He calls you a panini head.");
+  enemy.Gordon.gordonSpecialty.push("Verbal Assault! He calls you a useless sack of Yankee Doodle Dandy Shite.");
 
   enemy.Lovett = new enemy.Player2("Mrs. Lovett");
-  enemy.Lovett.attackSpecialty = "Meat Pie Attack! You ate some bad pie.";
+  enemy.Lovett.lovettSpecialty.push("Meat Pie Attack! You ate some bad pie.");
 
   enemy.Guy = new enemy.Player2("Guy Fiere");
-  enemy.Guy.attackSpecialty = "Offensive Appearance! You are sick from watching Guy Fiere eat.";
+  enemy.Guy.guySpecialty.push("Offensive Appearance! You are sick from watching Guy Fiere eat.");
 
   enemy.enemyPlayer = new FoodFight.Player2()
   return enemy;
